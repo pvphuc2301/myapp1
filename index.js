5c16a774731497c0c8cfc6337fd7ee9a125c205b
Phan Phuc <pvphuc2301@gmail.com>
2020-04-16 13:00:03 +0700

    modified index

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const Mouse = require('./mouse');
 const tom = new Cat(10);
 const jerry = new Mouse(4);
 
-console.log(tom);
-console.log(jerry);
-
+tom.eat(jerry);
 tom.eat(jerry);
 console.log(tom);
-console.log(jerry);
